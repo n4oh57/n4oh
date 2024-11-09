7a51c3311ab81b706ec0f4a937c9d69a980267cc
n4oh57 <toyzloh@gmail.com>
2024-11-09 12:34:37 +0800

    move armcc fputc retarget to new file.

--- sys/toolchain.c
+++ sys/toolchain.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "usart.h"
 /******************************************************************************/
 /************************** ARMCC compiler ************************************/
 
@@ -37,23 +36,3 @@ void _sys_exit(int x)
 
 #endif /* __MICROLIB */
 /******************************************************************************/
-
-#if defined ( __GNUC__ ) && !defined (__clang__) 
-/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
-   set to 'Yes') calls __io_putchar() */
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-/**
- * @brief  Retargets the C library printf function to the USART.
- * @param  None
- * @retval None
- */
-PUTCHAR_PROTOTYPE
-{
-	/* Place your implementation of fputc here */
-	/* e.g. write a character to the USART */
-	HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0x1000);
-	return ch;
-}
-#endif /* __GNUC__ */
